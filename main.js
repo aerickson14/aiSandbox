0321b7ea9bc8bfb6e69c9442edee2ac8fb2743c2
Ted Goddard <ted.goddard@robotsandpencils.com>
2023-09-11 16:48:04 -0600

    add stub chat handler

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
+
+const chatLines = []
+const transcript = document.getElementById('transcript')
+
+function pushTranscript(role, html) {
+  transcript.insertAdjacentHTML('beforeend', `<div class="wrapper"><div class="${role}">${html}</div></div>`)
+  transcript.scrollTop = transcript.scrollHeight
+}
+
+function askChatSystem(text) {
+  return `AI: ${text}`
+}
+
 function handleChatKey(event, input) {
-  console.log(event)
+
+  const key = event.key ?? 'Enter'
+  if (key !== 'Enter') {
+    return
+  }
+  
+  const text = input.value.trim()
+  
+  if (!text) {
+    return
+  }
+  if (text.trim().startsWith('/')) {
+    handleSlashCommand(text)
+    textinput.value = ""
+    return
+  }
+
+  chatLines.push({ role: "user", content: text })
+  textinput.value = ""
+
+  const answer = await askChatSystem(text)
+  if (answer) {
+    chatLines.push({ role: "assistant", content: answer })
+    pushTranscript('assistant', answer)
+  }
+
+
 }
 
 document.getElementById('chat-input').addEventListener('keyup', handleChatKey)
