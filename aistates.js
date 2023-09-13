fb470e4a713a96ab355a849affe544eb42323b64
Ted Goddard <ted.goddard@robotsandpencils.com>
2023-09-13 12:07:24 -0600

    parse state texts

--- aistates.js
+++ aistates.js
@@ -1,7 +1,17 @@
+const prompts = { }
+
+
 async function fetchState(name) {
   const response = await fetch(`states/${name}.txt`)
   const text = await response.text()
   return text
+}
+
+function parseStateText(text) {
+  const lines = text.split('\n')
+  const directives = lines.filter(line => line.match(/^[a-zA-Z]*:/))
+  const prompt = lines.filter(line => !line.match(/^[a-zA-Z]*:/))
+  return { directives, prompt }
 }
 
 export async function init() {
@@ -10,4 +20,10 @@ export async function init() {
   const stateNames = stateNamesText.trim().split('\n').map(name => name.trim())
   const stateTexts = await Promise.all(stateNames.map(fetchState))
   console.log("FETCHED", stateTexts)
+  const parsedPrompts = stateTexts.map(parseStateText)
+  console.log("PARSED", parsedPrompts)
+}
+
+export function getPrompt(name) {
+  return prompts[name]
 }
