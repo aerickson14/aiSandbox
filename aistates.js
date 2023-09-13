fda4987cd111585b441b2811caee90dd5f44129d
Ted Goddard <ted.goddard@robotsandpencils.com>
2023-09-13 11:16:39 -0600

    fetch state name texts

--- aistates.js
+++ aistates.js
@@ -1,1 +1,13 @@
+async function fetchState(name) {
+  const response = await fetch(`states/${name}.txt`)
+  const text = await response.text()
+  return text
+}
 
+export async function init() {
+  const response = await fetch('states/all.txt')
+  const stateNamesText = await response.text()
+  const stateNames = stateNamesText.trim().split('\n').map(name => name.trim())
+  const stateTexts = await Promise.all(stateNames.map(fetchState))
+  console.log("FETCHED", stateTexts)
+}
