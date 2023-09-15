107a1e98938a5dff031d1936a2b1800efcf7c150
Ted Goddard <ted.goddard.contractor@macmillan.com>
2023-09-15 13:51:49 -0600

    fix rubric bugs

--- rubrics.js
+++ rubrics.js
@@ -6,19 +6,6 @@ async function fetchRubric(name) {
   return { name, text }
 }
 
-function parseRubricText(textInfo) {
-  const { name, text } = textInfo
-  const lines = text.split('\n')
-  const directiveLines = lines.filter(line => line.match(/^[a-zA-Z]*:/))
-  const prompt = lines.filter(line => !line.match(/^[a-zA-Z]*:/)).join('\n')
-  const directives = { }
-  for (const directiveLine of directiveLines) {
-    const [match, type, info] = directiveLine.match(/(^[a-zA-Z]*):\s*(.*)$/)
-    addDirective(directives, type, info)
-  }
-  return { name, directives, prompt }
-}
-
 export function getRubric(name) {
   return rubrics[name]
 }
@@ -26,7 +13,7 @@ export function getRubric(name) {
 export async function init() {
   const response = await fetch('rubrics/all.txt')
   const rubricNamesText = await response.text()
-  const rubricNames = stateNamesText.trim().split('\n').map(name => name.trim())
+  const rubricNames = rubricNamesText.trim().split('\n').map(name => name.trim())
   const rubricInfos = await Promise.all(rubricNames.map(fetchRubric))
   console.log("FETCHED", rubricInfos)
   for (const rubricInfo of rubricInfos) {
